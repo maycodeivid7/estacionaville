7f1ae5d431c9537d0a1a7c7291d78326d08a1c1b
Mayco Deivid <mayco.deivid@webjoi.com.br>
2026-06-23 16:50:11 -0300

    Criação das funções iniciais dos botões

--- Estacionaville/Form1.cs
+++ Estacionaville/Form1.cs
@@ -16,5 +16,100 @@ namespace Estacionaville
         {
 
         }
+
+        private void btnVaga2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVaga20_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
