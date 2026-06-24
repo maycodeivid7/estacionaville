e9e67f0ec4a903876ce35dc76727a4e45cc17c23
Mayco Deivid <mayco.deivid@webjoi.com.br>
2026-06-24 10:05:31 -0300

    Inicialização do banco e criação da tabela

--- Estacionaville/Program.cs
+++ Estacionaville/Program.cs
@@ -11,6 +11,9 @@ namespace Estacionaville
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            Conexao.InicializarBanco();
+
             Application.Run(new Form1());
         }
     }
